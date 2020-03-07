d0d702bde77d91946a5c6671b34659c66739d221
David Detweiler <daviddetweiler1@gmail.com>
2020-03-07 11:07:57 -0800

    Add better import collection

--- ridl/src/main.rs
+++ ridl/src/main.rs
@@ -8,6 +8,8 @@ use std::path::PathBuf;
 
 // panic!() may not be the best of ideas
 
+// NOTE: Rust uses the leading "::" for global qualification
+
 fn get_ast(path: &Path) -> syn::File {
     let mut file = match File::open(path) {
         Ok(v) => v,
@@ -27,14 +29,19 @@ fn get_ast(path: &Path) -> syn::File {
     }
 }
 
-fn analyze(path: &Path) {
+struct IDLFile {
+    ast: syn::File,
+    deps: Vec<IDLFile>
+}
+
+fn find_deps(path: &Path) -> IDLFile {
     let ast = get_ast(path);
     let mut useds : Vec<syn::ItemUse> = Vec::new();
-    for item in ast.items {
+    for item in &ast.items {
         match item {
             syn::Item::Use(used) => {
                 println!("[INFO] Encountered use statement");
-                useds.push(used)
+                useds.push(used.clone())
             },
             syn::Item::Trait(tr) => (),
             syn::Item::Struct(st) => (),
@@ -59,15 +66,25 @@ fn analyze(path: &Path) {
         }
     }
 
+    let mut deps : Vec<IDLFile> = Vec::new();
     if imports.len() > 0 {
         println!("[INFO] Collecting ASTs for all imports of: {}", path.to_str().expect("[ERROR] Could not convert path to string"));
         for imp in imports {
             let mut buf = PathBuf::new();
             buf.push(path.parent().unwrap_or(Path::new("")));
             buf.push(imp + ".idl");
-            analyze(buf.as_path())
+            println!("\t{}", buf.to_str().expect("Could not convert from path to str"));
+            deps.push(find_deps(buf.as_path()))
         }
     }
+
+    // Once all imports have been "analyzed" (this could also involve code generation), analyze this file
+    // This is to allow for the whole type-checking thing
+    // Or we could split the functionality and have a recursive function build a tree of IDL ASTs
+    // Which we then traverse for analysis
+    // And again for generation (generation may need to have identifier table information for fully qualified names)
+
+    return IDLFile {ast, deps};
 }
 
 fn main() {
@@ -76,5 +93,5 @@ fn main() {
         println!("Usage: <invocation> <filepath>");
         return ()
     }
-    analyze(Path::new(&args[1]))
+    let tree = find_deps(Path::new(&args[1]));
 }
