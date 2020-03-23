49842a205294218801b5e1d0e3a4282f663b014b
David Detweiler <daviddetweiler1@gmail.com>
2020-03-22 17:11:40 -0700

    Add trait identification and inserting trait checks

--- ridl/src/main.rs
+++ ridl/src/main.rs
@@ -29,93 +29,238 @@ fn get_ast(path: &Path) -> syn::File {
     }
 }
 
-struct IDLFile {
-    ast: syn::File,
-    deps: Vec<usize>
-}
+// struct IDLFile {
+//     ast: syn::File,
+//     deps: Vec<usize>
+// }
 
-fn find_deps(path: &Path, idl_store: &mut Vec<IDLFile>, seen: &mut Vec<PathBuf>) -> usize {
-    match seen.iter().position(|x| x == path) {
-        Some(id) => {
-            println!("\tAlready saw IDL file at {:?}", seen[id]);
-            return id
-        },
-        None => ()
-    };
+// fn find_deps(path: &Path, idl_store: &mut Vec<IDLFile>, seen: &mut Vec<PathBuf>) -> usize {
+//     match seen.iter().position(|x| x == path) {
+//         Some(id) => {
+//             println!("\tAlready saw IDL file at {:?}", seen[id]);
+//             return id
+//         },
+//         None => ()
+//     };
 
-    let mut p = PathBuf::new();
-    p.push(path);
-    idl_store.push(IDLFile {ast: get_ast(path), deps: Vec::new()});
-    seen.push(p);
-    let id = idl_store.len() - 1;
+//     let mut p = PathBuf::new();
+//     p.push(path);
+//     idl_store.push(IDLFile {ast: get_ast(path), deps: Vec::new()});
+//     seen.push(p);
+//     let id = idl_store.len() - 1;
     
-    let mut imports : Vec<String> = Vec::new();
-    {
-        let idl = &idl_store[id];
-        let ast = &idl.ast;
-        let mut useds : Vec<syn::ItemUse> = Vec::new();
-        for item in &ast.items {
-            match item {
-                syn::Item::Use(used) => {
-                    println!("[INFO] Encountered use statement");
-                    useds.push(used.clone())
-                },
-                syn::Item::Trait(_) => (),
-                syn::Item::Struct(_) => (),
-                _ => {
-                    println!("[ERROR] IDL may only contain traits, structs, and use statements");
-                    panic!()
-                }
-            }
-        }
+//     let mut imports : Vec<String> = Vec::new();
+//     {
+//         let idl = &idl_store[id];
+//         let ast = &idl.ast;
+//         let mut useds : Vec<syn::ItemUse> = Vec::new();
+//         for item in &ast.items {
+//             match item {
+//                 syn::Item::Use(used) => {
+//                     println!("[INFO] Encountered use statement");
+//                     useds.push(used.clone())
+//                 },
+//                 syn::Item::Trait(_) => (),
+//                 syn::Item::Struct(_) => (),
+//                 _ => {
+//                     println!("[ERROR] IDL may only contain traits, structs, and use statements");
+//                     panic!()
+//                 }
+//             }
+//         }
 
-        for used in useds {
-            match used.tree {
-                syn::UseTree::Name(name) => {
-                    println!("[INFO] Used {}", name.ident);
-                    imports.push(name.ident.to_string())
-                },
-                _ => {
-                    println!("[ERROR] Only \"use <name>;\" is supported for use statements");
-                    panic!()
-                }
-            }
-        }
-    }    
+//         for used in useds {
+//             match used.tree {
+//                 syn::UseTree::Name(name) => {
+//                     println!("[INFO] Used {}", name.ident);
+//                     imports.push(name.ident.to_string())
+//                 },
+//                 _ => {
+//                     println!("[ERROR] Only \"use <name>;\" is supported for use statements");
+//                     panic!()
+//                 }
+//             }
+//         }
+//     }    
 
-    let mut deps : Vec<usize> = Vec::new();
-    if imports.len() > 0 {
-        println!(
-            "[INFO] Collecting ASTs for all imports of: {}",
-            path.to_str().expect("[ERROR] Could not convert path to string"));
+//     let mut deps : Vec<usize> = Vec::new();
+//     if imports.len() > 0 {
+//         println!(
+//             "[INFO] Collecting ASTs for all imports of: {}",
+//             path.to_str().expect("[ERROR] Could not convert path to string"));
         
-        for imp in imports {
-            let mut buf = PathBuf::new();
-            buf.push(path.parent().unwrap_or(Path::new("")));
-            buf.push(imp + ".idl");
-            println!("\t{}", buf.to_str().expect("Could not convert from path to str"));
-            deps.push(find_deps(buf.as_path(), idl_store, seen))
-        }
-    }
+//         for imp in imports {
+//             let mut buf = PathBuf::new();
+//             buf.push(path.parent().unwrap_or(Path::new("")));
+//             buf.push(imp + ".idl");
+//             println!("\t{}", buf.to_str().expect("Could not convert from path to str"));
+//             deps.push(find_deps(buf.as_path(), idl_store, seen))
+//         }
+//     }
 
-    idl_store[id].deps = deps;
+//     idl_store[id].deps = deps;
 
-    return idl_store.len() - 1;
+//     return idl_store.len() - 1;
+// }
+
+// fn build_sym_table(tree: usize, idls_store: &Vec<IDLFile>, table: &mut Vec<String>) {
+//     let idl = &idls_store[tree];
+//     for dep in &idl.deps {
+//         build_sym_table(*dep, idls_store, table);
+//     }
+//     for item in &idl.ast.items {
+//         match item {
+//             syn::Item::Trait(tr) => table.push(tr.ident.to_string()),
+//             syn::Item::Struct(st) => table.push(st.ident.to_string()),
+//             syn::Item::Use(_) => (),
+//             _ => panic!()
+//         }
+//     }
+// }
+
+enum Condition {
+    IsTrait,
+    IsCopy,
+    IRRefable, // Probably superseding IsCopy
+    None
 }
 
-fn build_sym_table(tree: usize, idls_store: &Vec<IDLFile>, table: &mut Vec<String>) {
-    let idl = &idls_store[tree];
-    for dep in &idl.deps {
-        build_sym_table(*dep, idls_store, table);
-    }
-    for item in &idl.ast.items {
-        match item {
-            syn::Item::Trait(tr) => table.push(tr.ident.to_string()),
-            syn::Item::Struct(st) => table.push(st.ident.to_string()),
-            syn::Item::Use(_) => (),
-            _ => panic!()
+struct Require {
+    id: usize,
+    needs: Condition
+}
+
+struct TypeCheck {
+    id: usize,
+    req_start: usize,
+    reqs: usize
+}
+
+struct TypeProps {
+    id: usize,
+    satisfies: Condition
+}
+
+impl Resolver {
+    fn new() -> Resolver {
+        Resolver {
+            checks: Vec::new(),
+            known: Vec::new(),
+            requires: Vec::new(),
+            ids: Vec::new()
         }
     }
+
+    fn resolve(&mut self, path: &Path) {
+        let ast = get_ast(path);
+        for item in &ast.items {
+            let mut check = TypeCheck {id: 0, req_start: self.requires.len(), reqs: 0};
+            check.id = match item {
+                syn::Item::Trait(tr) => self.process_trait(tr),
+                _ => {
+                    println!("[ERROR:IMPL] Not allowed");
+                    panic!();
+                }
+            };
+            check.reqs = self.requires.len() - check.req_start;
+            self.checks.push(check);
+        }
+        for check in &self.checks {
+            let ty = check.id;
+            println!("{}", self.ids[ty]);
+            for i in check.req_start..check.req_start + check.reqs {
+                let id = self.requires[i].id;
+                println!("\t\t{}", self.ids[id])
+            }
+        }
+    }
+
+    fn process_trait(&mut self, tr: &syn::ItemTrait) -> usize {
+        let tr_id = self.get_id(&tr.ident.to_string());
+        self.known.push(TypeProps {id: tr_id, satisfies: Condition::IsTrait});
+        for item in &tr.items {
+            match item {
+                syn::TraitItem::Method(m) => self.process_method(m),
+                _ => {
+                    println!("[ERROR] IDL traits may only have methods");
+                    panic!()
+                }
+            }
+        }
+        tr_id
+    }
+
+    fn process_method(&mut self, m: &syn::TraitItemMethod) {
+        for arg in &m.sig.inputs {
+            match arg {
+                syn::FnArg::Typed(ty) => self.add_require(&ty.ty),
+                syn::FnArg::Receiver(_) => ()
+            }
+        }
+        match &m.sig.output {
+            syn::ReturnType::Default => (),
+            syn::ReturnType::Type(_, ty) => self.add_require(&ty)
+        }
+    }
+
+    fn add_require(&mut self, ty: &syn::Type) {
+        match ty {
+            syn::Type::Reference(r) => self.add_trait_ref(&r),
+            _ => println!("[WARNING] Type checks only added for traits so far")
+        }
+    }
+
+    fn add_trait_ref(&mut self, r: &syn::TypeReference) {
+        let ty : &syn::Type = &r.elem;
+        match ty {
+            syn::Type::TraitObject(tr) => {
+                if tr.dyn_token.is_none() {
+                    println!("[ERROR] Only dynamic traits allowed");
+                    panic!()
+                }
+                if tr.bounds.len() != 1 {
+                    println!("[ERROR] Cannot bind multiple traits");
+                    panic!()
+                }
+                let name = match &tr.bounds[0] {
+                    syn::TypeParamBound::Trait(b) => {
+                        if b.path.segments.len() != 1 {
+                            println!("[ERROR:IMPL] Paths not supported at this time");
+                            panic!()
+                        }
+                        &b.path.segments[0]
+                    },
+                    _ => {
+                        println!("[ERROR] Must bind trait, not lifetime");
+                        panic!()
+                    }
+                };
+                let id = self.get_id(&name.ident.to_string());
+                self.requires.push(Require {id: id, needs: Condition::IsTrait})
+            },
+            _ => {
+                println!("[ERROR:IMPL] Now that's just wrong. . .")
+            }
+        }
+    }
+
+    fn get_id(&mut self, id: &String) -> usize {
+        let index = self.ids.iter().position(|v| v == id);
+        match index {
+            Some(v) => v,
+            None => {
+                self.ids.push(id.clone());
+                self.ids.len() - 1
+            }
+        }
+    }
+}
+
+struct Resolver {
+    checks: Vec<TypeCheck>,
+    known: Vec<TypeProps>,
+    requires: Vec<Require>,
+    ids: Vec<String>
 }
 
 fn main() {
@@ -124,11 +269,13 @@ fn main() {
         println!("Usage: <invocation> <filepath>");
         return ()
     }
-    let mut table : Vec<String> = Vec::new();
-    let mut idl_store : Vec<IDLFile> = Vec::new();
-    let mut seen : Vec<PathBuf> = Vec::new();
-    let tree = find_deps(Path::new(&args[1]), &mut idl_store, &mut seen);
-    // TODO: figure out how type checking is going to work in a dependency graph that itself has cycles
-    build_sym_table(tree, &idl_store, &mut table);
-    println!("Types found in tree: {:?}", table)
+    let path = Path::new(&args[1]);
+    let mut resolver = Resolver::new();
+    resolver.resolve(path);
+    // let mut table : Vec<String> = Vec::new();
+    // let mut idl_store : Vec<IDLFile> = Vec::new();
+    // let mut seen : Vec<PathBuf> = Vec::new();
+    // let tree = find_deps(Path::new(&args[1]), &mut idl_store, &mut seen);
+    //build_sym_table(tree, &idl_store, &mut table);
+    //println!("Types found in tree: {:?}", table)
 }
