9c3773b143428986ef2845e301eac0e9da0a2220
David Detweiler <daviddetweiler@gmail.com>
2020-05-22 02:43:48 +0000

    Add notes on create proxy preamble

--- ridl/src/creation_proxies.rs
+++ ridl/src/creation_proxies.rs
@@ -6,6 +6,38 @@ use std::io::{Read, Write};
 // Long story short, each method of the Create* proxies must generate the whole irq/free fn call stuff
 // plus a whole load of other crap
 
+/*
+    Either force all types to be globally qualified, or use this:
+
+    #![no_std]
+    #![feature(associated_type_defaults)]
+    extern crate alloc;
+    use alloc::boxed::Box;
+    use alloc::sync::Arc;
+    use syscalls::{Heap, Domain, Interrupt};
+    use usr::{bdev::BDev, vfs::VFS, xv6::Xv6, dom_a::DomA, dom_c::DomC, net::Net, pci::{PCI, PciBar, PciResource}};
+    use usr::error::Result;
+
+    And this is in the reference gen.rs:
+
+    use syscalls;
+    use create;
+    use proxy;
+    use usr;
+
+    use spin::Mutex;
+    use alloc::sync::Arc;
+    use alloc::boxed::Box;
+
+    use usr::error::Result;
+
+    use crate::domain::load_domain;
+    use crate::syscalls::{PDomain, Interrupt, Mmap};
+    use crate::heap::PHeap;
+    use crate::interrupt::{disable_irq, enable_irq};
+    use crate::thread;
+*/
+
 fn process_create_signature(m: &syn::TraitItemMethod, file: &mut File) -> Result<()> {
     let sig = &m.sig;
     let sig_str = quote::quote!(#sig).to_string();
