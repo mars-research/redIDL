adb2ac91567f1e2e13d62bf17ce0dcf0cffe1264
David Detweiler <daviddetweiler1@gmail.com>
2020-09-22 14:07:44 -0700

    Improve error context

--- red_idl/src/main.rs
+++ red_idl/src/main.rs
@@ -17,20 +17,15 @@ struct PruningVisitor {
     context: Vec<String>
 }
 
-trait TraitTest {
-    fn foo(a: fn(u32) -> u32);
-}
-
 impl PruningVisitor {
     fn get_context(&self) -> String {
-        let mut msg = String::new();
         if self.context.len() == 0 {
-            msg
+            "<global scope>".to_string()
         }
         else {
-            msg = format!("{}", self.context[0]);
+            let mut msg = format!("{}", self.context[0]);
             for extra in &self.context[1..] {
-                msg = format!("{}::{}", msg, extra)
+                msg = format!("{}, {}", msg, extra)
             }
 
             msg
@@ -54,14 +49,14 @@ impl<'ast> Visit<'ast> for PruningVisitor {
     }
 
     fn visit_item_struct(&mut self, i: &'ast syn::ItemStruct) {
-        self.context.push(i.ident.to_string());
+        self.context.push(format!("struct {}", i.ident.to_string()));
         visit::visit_item_struct(self, i);
         self.context.pop();
     }
 
     fn visit_field(&mut self, i: &'ast syn::Field) {
         match &i.ident {
-            Some(id) => self.context.push(id.to_string()),
+            Some(id) => self.context.push(format!("field {}", id.to_string())),
             None => self.context.push("<unnamed field>".to_string())
         }
 
@@ -70,14 +65,26 @@ impl<'ast> Visit<'ast> for PruningVisitor {
     }
 
     fn visit_item_trait(&mut self, i: &'ast syn::ItemTrait) {
-        self.context.push(i.ident.to_string());
+        self.context.push(format!("trait {}", i.ident.to_string()));
         visit::visit_item_trait(self, i);
         self.context.pop();
     }
 
     fn visit_signature(&mut self, i: &'ast syn::Signature) {
-        self.context.push(i.ident.to_string());
+        self.context.push(format!("fn {}", i.ident.to_string()));
         visit::visit_signature(self, i);
+        self.context.pop();
+    }
+
+    fn visit_expr_closure(&mut self, i: &'ast syn::ExprClosure) {
+        println!("\x1b[31merror:\x1b[0m Closures are not permitted");
+        println!("at: {}", self.get_context());
+        visit::visit_expr_closure(self, i);
+    }
+
+    fn visit_item_const(&mut self, i: &'ast syn::ItemConst) {
+        self.context.push(format!("const {}", i.ident.to_string()));
+        visit::visit_item_const(self, i);
         self.context.pop();
     }
 }
