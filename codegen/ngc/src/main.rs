2d3723536393de51262fca37be77f6a39a2ed32d
Tianjiao Huang <git@tjhu.dev>
2021-04-26 17:21:22 -0700

    attempt domain create

--- codegen/ngc/src/main.rs
+++ codegen/ngc/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(option_expect_none, box_syntax, box_patterns, option_unwrap_none)]
 
-mod domain_creation;
+mod domain_create;
 mod proxy;
 mod type_resolution;
 #[macro_use]
@@ -16,7 +16,7 @@ use std::io::Read;
 use std::process::Command;
 
 use clap::{App, Arg, ArgMatches};
-use log::info;
+use log::{info, warn};
 use quote::quote;
 use syn::{parse_quote, Item, ItemMod, Meta, NestedMeta, Type};
 
@@ -27,19 +27,24 @@ fn main() {
     // Parse arguments and run the compiler
     let matches = App::new("Proxy Generator")
         .version(env!("CARGO_PKG_VERSION"))
-        .about("RedIDL New Generation Codegenerator.")
+        .about("RedIDL New Generation Compiler(NGC).")
         .arg(
             Arg::with_name("INPUT")
-                .help("Sets the input file.")
-                // .required(true)
-                .index(1),
+            .help("Path to the interface file.")
+            .required(true)
+            .index(1),
         )
         .arg(
             Arg::with_name("OUTPUT")
-                .help("Sets the output file.")
-                // .required(true)
-                .index(2),
+            .help("Path to the output file.")
+            .required(true)
+            .index(2),
         )
+        .arg(Arg::with_name("domain_create_output")
+                               .value_name("domain_create_output")
+                               .long("domain_create_output")
+                               .help("Path to the domain create generation output.")
+                               .takes_value(true))
         .get_matches();
 
     run(&matches).unwrap();
@@ -49,7 +54,7 @@ fn run(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let input_path = args.value_of("INPUT").unwrap();
     let output_path = args.value_of("OUTPUT").unwrap();
     info!("Running redIDL on {}", input_path);
-    let mut file = File::open(&input_path).unwrap();
+    let mut file = File::open(&input_path).expect("Failed to open input file");
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
 
@@ -58,10 +63,23 @@ fn run(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
     // Clean the file
     remove_prelude(&mut ast);
 
-    // Generate code in place
-    generate(&mut ast);
+    // Generate code.
+    let generated_domain_create = generate(&mut ast);
 
-    // Write output
+    // Write generated proxy.
+    write_ast_to_file(&ast, output_path);
+
+    // Write generated domain create.
+    if let Some(domain_create_out) = args.value_of("domain_create_output") {
+        let domain_create_ast: syn::File = parse_quote!{
+            #(#generated_domain_create)*
+        };
+
+        write_ast_to_file(&ast, output_path)
+    }
+
+
+    info!("Writting interface output to {}", output_path);
     let output = quote!(#ast).to_string();
     std::fs::write(&output_path, output).unwrap();
 
@@ -71,12 +89,13 @@ fn run(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
         .arg(format!("rustfmt {}", &output_path))
         .output();
 
-    info!("Output is written to {}", output_path);
 
     Ok(())
 }
 
-fn generate(ast: &mut syn::File) {
+// Generate proxy and other stuff from `items` in place.
+// Save Returns domain create generation.
+fn generate(ast: &mut syn::File) -> Vec<syn::Item> {
     // Generate type id
     crate::type_resolution::generate_typeid(ast);
 
@@ -85,14 +104,18 @@ fn generate(ast: &mut syn::File) {
     generate_recurse(&mut ast.items, &mut module_path)
 }
 
-fn generate_recurse(items: &mut Vec<syn::Item>, module_path: &mut Vec<syn::Ident>) {
+// Generate proxy and other stuff from `items` in place, recursively.
+// Returns domain create generation.
+fn generate_recurse(items: &mut Vec<syn::Item>, module_path: &mut Vec<syn::Ident>) -> Vec<syn::Item> {
     let mut generated_items = Vec::<syn::Item>::new();
+    let mut generated_domain_create_items = Vec::<syn::Item>::new();
     for item in items.iter_mut() {
         match item {
             Item::Mod(md) => {
                 if let Some((_, items)) = &mut md.content {
+                    // Recursive into the submodule.
                     module_path.push(md.ident.clone());
-                    generate_recurse(items, module_path);
+                    generated_domain_create_items.extend(generate_recurse(items, module_path));
                     module_path.pop();
                 }
             }
@@ -104,16 +127,20 @@ fn generate_recurse(items: &mut Vec<syn::Item>, module_path: &mut Vec<syn::Ident
 
                 // Attempt to generate domain creation
                 if let Some(generated) =
-                    crate::domain_creation::generate_domain_creation(tr, module_path)
+                    crate::domain_create::generate_domain_create(tr, module_path)
                 {
-                    generated_items.extend(generated);
+                    generated_domain_create_items.push(generated);
                 }
             }
             _ => {}
         }
     }
 
+    // Insert the generated proxy inplace.
     items.extend(generated_items);
+
+    // Return the generated domain create.
+    generated_domain_create_items
 }
 
 /// Remove unwanted stuff generated by cargo-expand
@@ -165,4 +192,20 @@ fn remove_prelude(ast: &mut syn::File) {
 
         true
     });
+}
+
+// Write ast to file and run formatter.
+fn write_ast_to_file(ast: &syn::File, output_path: &str) {
+     // Write output
+     info!("Writting output to {}", output_path);
+     let output = quote!(#ast).to_string();
+     std::fs::write(&output_path, output).expect("Failed to write output file");
+ 
+     // Format output file
+     if let Err(err) =  Command::new("bash")
+         .arg("-c")
+         .arg(format!("rustfmt {}", &output_path))
+         .output() {
+            warn!("Failed to run formatter on output file {}. Formatting is skipped. Error {}", output_path, err)
+    }
 }
