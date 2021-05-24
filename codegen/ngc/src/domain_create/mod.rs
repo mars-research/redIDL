6246636f638e94b15ab2aa40e636b3e79dcb6280
Tianjiao Huang <git@tjhu.dev>
2021-05-24 11:21:31 -0700

    blob creation (I think?)

--- codegen/ngc/src/domain_create/mod.rs
+++ codegen/ngc/src/domain_create/mod.rs
@@ -1,13 +1,17 @@
+mod blob_domain_create;
+mod linked_domain_create;
+
 use std::collections::HashMap;
-
 use crate::{has_attribute, remove_attribute};
-
 use log::info;
 use quote::format_ident;
-
 use syn::{Expr, FnArg, Ident, ImplItemMethod, Item, ItemFn, ItemTrait, Lit, Path, TraitItem, TraitItemMethod, parse_quote};
 
-const DOMAIN_CREATE_ATTR: &str = "domain_create";
+
+
+pub const LINKED_DOMAIN_CREATE_ATTR: &str = "domain_create";
+pub const BLOB_DOMAIN_CREATE_ATTR: &str = "blob_domain_create";
+
 
 /// Generation of domain create.
 /// It also keep track of all the domain create it generates.
@@ -24,8 +28,18 @@ impl DomainCreateBuilder {
 
     /// Generates the domain create for `input` if it has the `DOMAIN_CREATE_ATTR` attribute.
     pub fn generate_domain_create(&mut self, input: &mut ItemTrait, module_path: &[Ident]) -> Option<Vec<Item>> {
-        // Only traits with `DOMAIN_CREATE_ATTR` will be processed.
-        if !has_attribute!(input, DOMAIN_CREATE_ATTR) {
+        // Create an attribute map.
+        let attrs: HashMap<String, Option<Lit>> = crate::utils::create_attribue_map(&input.attrs);
+
+        // Filter out non-domain_create traits and remove domain_create attributes.
+        let is_blob_domain_create;
+        if has_attribute!(input, LINKED_DOMAIN_CREATE_ATTR) {
+            is_blob_domain_create = false;
+            remove_attribute!(input, LINKED_DOMAIN_CREATE_ATTR);
+        } else if has_attribute!(input, BLOB_DOMAIN_CREATE_ATTR) {
+            is_blob_domain_create = true;
+            remove_attribute!(input, BLOB_DOMAIN_CREATE_ATTR);
+        } else {
             return None;
         }
     
@@ -38,15 +52,11 @@ impl DomainCreateBuilder {
         let mut input_copy = input.clone();
         crate::path_refactoring::refactor_path_in_trait(&format_ident!("crate"), &format_ident!("interface"), &mut input_copy);
     
-        // Remove the interface attribute and add a comment so we know it's an domain_create
-        remove_attribute!(input, DOMAIN_CREATE_ATTR);
+        // Add a comment so we know it's generated.
         input.attrs.push(
             parse_quote! {#[doc = "redIDL Auto Generated: domain_create trait. Generations are below"]},
         );
         let input = input_copy;
-    
-        // Create an attribute map
-        let attrs: HashMap<String, Option<Lit>> = crate::utils::create_attribue_map(&input.attrs);
     
         // Extract the domain path.
         let domain_path = crate::expect!(
@@ -66,7 +76,11 @@ impl DomainCreateBuilder {
             .iter()
             .map(|item| match item {
                 TraitItem::Method(method) => {
-                    generate_domain_create_for_trait_method(&domain_path, method)
+                    if is_blob_domain_create {
+                        self::blob_domain_create::generate_domain_create_for_trait_method(&domain_path, method)
+                    } else {
+                        self::linked_domain_create::generate_domain_create_for_trait_method(&domain_path, method)
+                    }
                 }
                 _ => unimplemented!("Non-method member found in trait {:#?}", input),
             })
@@ -165,125 +179,3 @@ impl DomainCreateBuilder {
     }
 }
 
-
-/// This generates a public fn and a impl method.
-/// This public fn is exposed to the kernel while the impl method is exposed to the users.
-fn generate_domain_create_for_trait_method(
-    domain_path: &str,
-    method: &TraitItemMethod,
-) -> (syn::ImplItemMethod, syn::ItemFn) {
-    // Remove `self` from the argument list
-    let selfless_args: Vec<_> = method
-        .sig
-        .inputs
-        .iter()
-        .filter(|arg| match arg {
-            FnArg::Receiver(_) => false,
-            FnArg::Typed(_) => true,
-        })
-        .collect();
-
-    // Extract essential variables for generation.
-    let method_ident = &method.sig.ident;
-    let _method_args = method.sig.inputs.iter().collect::<Vec<_>>();
-    let method_sig = &method.sig;
-    let canonicalized_domain_path = domain_path.replace("/", "_");
-    let generated_fn_ident = format_ident!("{}_{}", canonicalized_domain_path, method_ident);
-    let domain_start_ident = format_ident!("_binary_domains_build_{}_start", canonicalized_domain_path);
-    let domain_end_ident = format_ident!("_binary_domains_build_{}_end", canonicalized_domain_path);
-    let rtn = &method.sig.output;
-    let ep_rtn = match &method.sig.output {
-        syn::ReturnType::Type(_, ty) => match ty {
-            box syn::Type::Tuple(tuple) => {
-                assert_eq!(tuple.elems.iter().count(), 2, "Expecting a tuple of two in the return type of method {:?} of domain {:?}", method_ident, domain_path);
-                tuple.elems.iter().nth(1).unwrap()
-            },
-            _ => panic!("Expecting a tuple of two in the return type of method {:?} of domain {:?}", method_ident, domain_path),
-        },
-        syn::ReturnType::Default => panic!("Method {:?} of domain {:?} does not have a return type. Expecting a tuple of two.", method_ident, domain_path),
-    };
-
-    // Generate impl method.
-    let generated_impl = parse_quote! {
-        #method_sig {
-            // Entering kernel, disable irq
-            crate::interrupt::disable_irq();
-
-            let rtn_ = #generated_fn_ident(#(#selfless_args),*);
-
-            // Leaving kernel, reable irq
-            crate::interrupt::enable_irq();
-
-            // Returns the domain to caller.
-            rtn_
-        }
-    };
-
-
-    // Generated fn.
-    let mut fn_sig = method_sig.clone();
-    fn_sig.ident = generated_fn_ident.clone();
-    let generated_fn = parse_quote! {
-        pub(crate) fn #generated_fn_ident(#(#selfless_args),*) #rtn {
-            // Entering kernel, disable irq
-            crate::interrupt::disable_irq();
-
-            extern "C" {
-                fn #domain_start_ident();
-                fn #domain_end_ident();
-            }
-
-            let binary_range_ = (
-                #domain_start_ident as *const u8,
-                #domain_end_ident as *const u8,
-            );
-
-            type UserInit_ =
-                fn(::alloc::boxed::Box<dyn ::syscalls::Syscall>, ::alloc::boxed::Box<dyn ::syscalls::Heap>, #(#selfless_args),*) -> #ep_rtn;
-
-            let (dom_, entry_) = unsafe { crate::domain::load_domain(#domain_path, binary_range_) };
-
-            // Type cast the pointer to entry point to the correct type.
-            let user_ep_: UserInit_ = unsafe { ::core::mem::transmute::<*const (), UserInit_>(entry_) };
-
-            let pdom_ = ::alloc::boxed::Box::new(crate::syscalls::PDomain::new(::alloc::sync::Arc::clone(&dom_)));
-            let pheap_ = ::alloc::boxed::Box::new(crate::heap::PHeap::new());
-
-            // update current domain id.
-            let thread_ = crate::thread::get_current_ref();
-            let old_id_ = {
-                let mut thread = thread_.lock();
-                let old_id = thread.current_domain_id;
-                thread.current_domain_id = dom_.lock().id;
-                old_id
-            };
-
-            // Enable interrupts on exit to user so it can be preempted.
-            crate::interrupt::enable_irq();
-            // Jumps to the domain entry point.
-            let ep_rtn_ = user_ep_(pdom_, pheap_, #(#selfless_args),*);
-            // Disable interrupts as we are back to the kernel.
-            crate::interrupt::disable_irq();
-
-            // change domain id back
-            {
-                thread_.lock().current_domain_id = old_id_;
-            }
-
-            #[cfg(feature = "domain_create_log")]
-            println!("domain/{}: returned from entry point", #domain_path);
-
-            // Setup the return object.
-            let dom_: ::alloc::boxed::Box<dyn ::syscalls::Domain> = ::alloc::boxed::Box::new(crate::syscalls::PDomain::new(::alloc::sync::Arc::clone(&dom_)));
-            let rtn_ = (dom_, ep_rtn_);
-
-            // Leaving kernel, reable irq
-            crate::interrupt::enable_irq();
-
-            // Returns the domain to caller.
-            rtn_
-        }
-    };
-
-    (generated_impl, generated_fn)
-}
