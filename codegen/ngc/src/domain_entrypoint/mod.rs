3ee825ad3dabe15026543ca02ea597ca803cadc5
Connor Zwick <cozwick@uci.edu>
2021-12-15 02:30:36 +0000

    Added main_rs generation

--- codegen/ngc/src/domain_entrypoint/mod.rs
+++ codegen/ngc/src/domain_entrypoint/mod.rs
@@ -1,25 +1,30 @@
 use crate::domain_create::DomainCreateComponent;
 use std::{
     fs::{canonicalize, create_dir, remove_dir_all, File},
+    iter::Map,
     path::{Path, PathBuf},
 };
 
 use log::{debug, error, info, warn};
-use quote::ToTokens;
-use syn::TraitItemMethod;
+use quote::{format_ident, ToTokens};
+use syn::{parse_quote, TraitItemMethod};
 
 pub struct DomainEntrypointFactory {
     output_folder_created: bool,
+    output_folder_path: PathBuf,
 
     domains_folder: PathBuf,
 }
 
 impl DomainEntrypointFactory {
     pub fn new(domains_folder: PathBuf) -> Self {
-        Self::setup_output_folder(&domains_folder.join("generated"));
+        let output_folder_path = &domains_folder.join("generated");
+
+        Self::setup_output_folder(output_folder_path);
 
         DomainEntrypointFactory {
             output_folder_created: true,
+            output_folder_path,
             domains_folder,
         }
     }
@@ -100,32 +105,127 @@ impl DomainEntrypointFactory {
         )
     }
 
+    pub fn generate_entrypoint_main_rs(
+        &self,
+        domain_name: &str,
+        domain_components: &Vec<DomainCreateComponent>,
+        method: &TraitItemMethod,
+    ) -> String {
+        let domain_components_args = domain_components
+            .iter()
+            .map(|comp| match comp {
+                &DomainCreateComponent::Domain => {
+                    parse_quote! {s: Box<dyn syscalls::Syscall + Send + Sync>}
+                }
+                &DomainCreateComponent::Heap => {
+                    parse_quote! {heap: Box<dyn syscalls::Heap + Send + Sync>}
+                }
+                &DomainCreateComponent::MMap => {
+                    parse_quote! {mmap: Box<dyn syscalls::Mmap + Send + Sync}
+                }
+            })
+            .collect::<Vec<syn::FnArg>>();
+
+        let domain_components_init_statements = domain_components.iter().map(|comp| match comp {
+            &DomainCreateComponent::Domain => {
+                parse_quote! {  libsyscalls::syscalls::init(s); }
+            }
+            &DomainCreateComponent::Heap => {
+                parse_quote! {  interface::rref::init(heap, libsyscalls::syscalls::sys_get_current_domain_id()); }
+            }
+            &DomainCreateComponent::MMap => {
+                parse_quote! {  libsyscalls::syscalls::init_mmap(m);}
+            }
+        }).collect::<Vec<syn::Stmt>>();
+
+        let other_domain_args = method
+            .sig
+            .inputs
+            .iter()
+            .filter(|arg| match arg {
+                syn::FnArg::Typed(pat) => true,
+                syn::FnArg::Receiver(_) => false,
+            })
+            .collect::<Vec<_>>();
+
+        let domain_args = domain_components_args
+            .iter()
+            .chain(other_domain_args.clone())
+            .collect::<Vec<_>>();
+
+        let domain_args_idents = other_domain_args.iter().filter_map(|a| match a {
+            syn::FnArg::Typed(t) => match t.pat.as_ref() {
+                syn::Pat::Ident(id) => Some(&id.ident),
+                _ => {
+                    warn!("Unsupported argument identity");
+                    None
+                }
+            },
+            _ => None,
+        });
+
+        let domain_ident = format_ident!("{}", domain_name);
+
+        let domain_return_type = match &method.sig.output {
+            syn::ReturnType::Type(arrow, return_type) => match return_type.as_ref() {
+                syn::Type::Tuple(tuple_type) => {
+                    let iter = tuple_type.elems.iter();
+                    if iter.len() <= 1 {
+                        panic!("Return Type doesn't have enough elements, minimum of two required");
+                    }
+
+                    let returned_types = iter.skip(1);
+                    let return_type: syn::Type = parse_quote! {
+                        Box<( #(#returned_types),* )>
+                    };
+
+                    return_type
+                }
+                _ => panic!("Domain must return a tuple!"),
+            },
+            syn::ReturnType::Default => panic!("Domain must return a tuple!"),
+        };
+
+        let main_rs: syn::File = parse_quote!(
+            #![no_std]
+            #![no_main]
+
+            extern crate alloc;
+
+            use alloc::boxed::Box;
+            use console::println;
+
+            use #domain_ident;
+
+            #[no_mangle]
+            pub fn trusted_entry(
+                #(#domain_args),*
+            ) -> #domain_return_type {
+                #(#domain_components_init_statements)*
+
+                #domain_ident::main(#(#domain_args_idents),*)
+            }
+        );
+
+        return main_rs.to_token_stream().to_string();
+    }
+
+    pub fn write_entrypoint_crate(&self, crate_name: &str, cargo_toml: String, main_rs: String) {
+        let output_path = self.output_folder_path.join(crate_name + "_entry_point");
+
+        std::fs::create_dir(output_path);
+        std::fs::write(output_path.join("Cargo.toml"), cargo_toml);
+
+        std::fs::create_dir(output_path.join("src"));
+        std::fs::write(output_path.join("src/main.rs"), main_rs);
+    }
+
     pub fn generate_domain_entrypoint_crates(
         &self,
         domain_relative_path: &Path,
         domain_components: &Vec<DomainCreateComponent>,
         method: &TraitItemMethod,
     ) {
-        // debug!(
-        //     "domain_relative_path: {:#?}, domain_components: {:#?}",
-        //     domain_relative_path, domain_components,
-        // );
-
-        // debug!("method name: {:#?}", method.sig.ident.to_string());
-
-        // debug!(
-        //     "arguments: {:#?}",
-        //     method
-        //         .sig
-        //         .inputs
-        //         .iter()
-        //         .filter_map(|arg| match arg {
-        //             syn::FnArg::Typed(a) => Some(a.to_token_stream().to_string()),
-        //             _ => None,
-        //         })
-        //         .collect::<Vec<_>>()
-        // );
-
         let method_name = method.sig.ident.to_string();
         if !method_name.starts_with("create_domain_") {
             warn!(
@@ -139,5 +239,11 @@ impl DomainEntrypointFactory {
 
         let cargo_toml = self.generate_entrypoint_cargo(domain_relative_path, domain_name);
         debug!("CARGO TOML: {:}", cargo_toml);
+
+        let main_rs = self.generate_entrypoint_main_rs(domain_name, domain_components, method);
+        debug!("main.rs: {:}", main_rs);
+
+        self.write_entrypoint_crate(domain_name, cargo_toml, main_rs);
+        panic!("END");
     }
 }
